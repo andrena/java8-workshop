72142ad7dd1c405e45af13f7b449e2d9344f4bd5
Marc Philipp <marc.philipp@andrena.de>
2014-03-12 20:58:03 +0000

    Consumer Demo

---
+++ test/de/andrena/java8/functional/consumers/ConsumerDemo4.java
@@ -0,0 +1,23 @@
+package de.andrena.java8.functional.consumers;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+import de.andrena.java8.Person;
+
+public class ConsumerDemo4 {
+
+	public static void main(String[] args) {
+		Person antonio = new Person("Antonio", "Calabrese");
+		Person julia = new Person("Julia", "Hall", LocalDate.of(1978, 3, 25));
+		Person stefan = new Person("Stefan", "Maier");
+
+		List<Person> personen = new ArrayList<>(Arrays.asList(antonio, julia, stefan));
+
+		Consumer<Person> consumer = person -> System.out.println(person);
+		personen.forEach(consumer);
+	}
+}
---
+++ test/de/andrena/java8/functional/consumers/ConsumerDemo5.java
@@ -0,0 +1,21 @@
+package de.andrena.java8.functional.consumers;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import de.andrena.java8.Person;
+
+public class ConsumerDemo5 {
+
+	public static void main(String[] args) {
+		Person antonio = new Person("Antonio", "Calabrese");
+		Person julia = new Person("Julia", "Hall", LocalDate.of(1978, 3, 25));
+		Person stefan = new Person("Stefan", "Maier");
+
+		List<Person> personen = new ArrayList<>(Arrays.asList(antonio, julia, stefan));
+
+		personen.forEach(person -> System.out.println(person));
+	}
+}
