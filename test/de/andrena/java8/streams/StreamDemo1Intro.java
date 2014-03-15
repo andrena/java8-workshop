edc650b361d9eaa9196b389cdde303a41c83e46b
David Burkhart <David.Burkhart@andrena.de>
2014-03-15 11:35:21 +0000

    Streams: Anderer Intro-Test; Folien überarbeitet

--- test/de/andrena/java8/streams/StreamDemo1Intro.java
+++ test/de/andrena/java8/streams/StreamDemo1Intro.java
@@ -1,6 +1,14 @@
 package de.andrena.java8.streams;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
@@ -35,5 +43,36 @@ public class StreamDemo1Intro extends TestMit100000Personen {
 				.anyMatch(adresse -> adresse.getStadt().equals("Stuttgart"));
 
 		assertTrue(esGibtJemandAusStuttgartDer2002GeborenWurde);
+	}
+
+	@Test
+	public void loopingIsUgly2() {
+		List<String> nachnamenDie2002GeburtstagHaben = new ArrayList<>();
+		for (Person person : personen) {
+			if (person.getGeburtstag().getYear() == 2002) {
+				if (!nachnamenDie2002GeburtstagHaben.contains(person.getNachname())) {
+					nachnamenDie2002GeburtstagHaben.add(person.getNachname());
+				}
+			}
+		}
+		nachnamenDie2002GeburtstagHaben.sort(Comparator.naturalOrder());
+
+		assertThat(nachnamenDie2002GeburtstagHaben, hasSize(100));
+		assertThat(nachnamenDie2002GeburtstagHaben.get(0), is("Albrecht"));
+		assertThat(nachnamenDie2002GeburtstagHaben.get(99), is("Zimmermann"));
+	}
+
+	@Test
+	public void streamingIsBeautiful2() {
+		List<String> nachnamenDie2002GeburtstagHaben = personen.stream()
+				.filter(person -> person.getGeburtstag().getYear() == 2002) //
+				.map(person -> person.getNachname()) //
+				.distinct() //
+				.sorted() //
+				.collect(Collectors.toList());
+
+		assertThat(nachnamenDie2002GeburtstagHaben, hasSize(100));
+		assertThat(nachnamenDie2002GeburtstagHaben.get(0), is("Albrecht"));
+		assertThat(nachnamenDie2002GeburtstagHaben.get(99), is("Zimmermann"));
 	}
 }
