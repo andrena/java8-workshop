6d19f398f3e334e4b19979757f5f53683f5bd781
David Burkhart <David.Burkhart@andrena.de>
2014-03-17 20:56:35 +0000

    DateTime API

---
+++ test/de/andrena/java8/time/DateTimeDemo4PeriodAndDuration.java
@@ -0,0 +1,37 @@
+package de.andrena.java8.time;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+import org.junit.Test;
+
+public class DateTimeDemo4PeriodAndDuration {
+
+	@Test
+	public void period() {
+		Period einMonat = Period.ofMonths(1);
+		LocalDateTime endeFebruar = LocalDateTime.of(2014, 3, 31, 0, 0).minus(einMonat);
+		assertThat(endeFebruar, is(LocalDateTime.of(2014, 2, 28, 0, 0)));
+	}
+
+	@Test
+	public void duration() {
+		Duration einMonat = ChronoUnit.MONTHS.getDuration();
+		LocalDateTime endeFebruar = LocalDateTime.of(2014, 3, 31, 0, 0).minus(einMonat);
+		assertThat(endeFebruar, is(LocalDateTime.of(2014, 2, 28, 13, 30, 54)));
+	}
+
+	@Test
+	public void weihnachten() throws Exception {
+		LocalDate weihnachten = LocalDate.now().with(Month.DECEMBER).withDayOfMonth(24);
+		Period period = LocalDate.now().until(weihnachten);
+		System.out.println("Noch " + period.getMonths() + " Monate und " + period.getDays() + " Tage bis Weihnachten");
+	}
+}
